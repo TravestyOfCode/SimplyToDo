e45f7e38f41eca1e03a38c2724cb6794f1cb9cc2
travis@travestyofcode.com <travis@travestyofcode.com>
2025-06-21 05:20:09 -0400

    Add project files.

---
+++ SimplyToDo.Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace SimplyToDo.Data;
+
+internal class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+    }
+}
+
+internal class AppDbContextDesignTimeFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .Build();
+
+        var connString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Unable to get connection string.");
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer(connString);
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
---
+++ SimplyToDo.Data/Services/DataConfigService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimplyToDo.Data.Services;
+
+public static class DataConfigService
+{
+    public static IServiceCollection AddDataServices(this IServiceCollection services, string connectionString)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(connectionString);
+
+        services.AddDbContext<AppDbContext>(o => o.UseSqlServer(connectionString))
+            .AddDefaultIdentity<AppUser>(o => o.SignIn.RequireConfirmedAccount = true)
+            .AddDefaultTokenProviders()
+            .AddEntityFrameworkStores<AppDbContext>();
+
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddMediatR(c => c.RegisterServicesFromAssembly(System.Reflection.Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
