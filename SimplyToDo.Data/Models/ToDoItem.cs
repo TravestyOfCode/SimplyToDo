5204d534403aa97a82270596a63b4e72f0c2d2ab
travis@travestyofcode.com <travis@travestyofcode.com>
2025-06-21 05:39:45 -0400

    Adds List and Item models with static projection methods.

---
+++ SimplyToDo.Data/Models/ToDoItem.cs
@@ -0,0 +1,39 @@
+﻿namespace SimplyToDo.Data.Models;
+
+public class ToDoItem
+{
+    public int Id { get; set; }
+    public int ToDoListId { get; set; }
+    public int Ordinal { get; set; }
+    public bool IsCompleted { get; set; }
+    public required string Description { get; set; }
+    public DateOnly? DueDate { get; set; }
+    public TimeOnly? DueTime { get; set; }
+}
+
+internal static class ToDoItemExtensions
+{
+    public static IQueryable<ToDoItem> ProjectToModel(this IQueryable<Entities.ToDoItem> source) =>
+        source.Select(p => new ToDoItem()
+        {
+            Id = p.Id,
+            ToDoListId = p.ToDoListId,
+            Ordinal = p.Ordinal,
+            IsCompleted = p.IsCompleted,
+            Description = p.Description,
+            DueDate = p.DueDate,
+            DueTime = p.DueTime
+        });
+
+    public static ToDoItem AsModel(this Entities.ToDoItem source) =>
+         new ToDoItem()
+         {
+             Id = source.Id,
+             ToDoListId = source.ToDoListId,
+             Ordinal = source.Ordinal,
+             IsCompleted = source.IsCompleted,
+             Description = source.Description,
+             DueDate = source.DueDate,
+             DueTime = source.DueTime
+         };
+}
---
+++ SimplyToDo.Data/Models/ToDoList.cs
@@ -0,0 +1,33 @@
+﻿namespace SimplyToDo.Data.Models;
+
+public class ToDoList
+{
+    public int Id { get; set; }
+    public required string OwnerId { get; set; }
+    public int Ordinal { get; set; }
+    public required string Title { get; set; }
+    public List<ToDoItem>? Items { get; set; }
+}
+
+internal static class ToDoListExtensions
+{
+    public static IQueryable<ToDoList> ProjectToModel(this IQueryable<Entities.ToDoList> source) =>
+        source.Select(p => new ToDoList()
+        {
+            Id = p.Id,
+            OwnerId = p.OwnerId,
+            Ordinal = p.Ordinal,
+            Title = p.Title,
+            Items = p.Items == null ? null : p.Items.Select(i => i.AsModel()).ToList()
+        });
+
+    public static ToDoList AsModel(this Entities.ToDoList source) =>
+        new ToDoList()
+        {
+            Id = source.Id,
+            OwnerId = source.OwnerId,
+            Ordinal = source.Ordinal,
+            Title = source.Title,
+            Items = source.Items?.Select(i => i.AsModel()).ToList()
+        };
+}
