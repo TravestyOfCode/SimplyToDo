4c568d507cc45366036b9d0f2eaa0ef6366beddd
travis@travestyofcode.com <travis@travestyofcode.com>
2026-06-27 18:53:06 -0400

    Adds ToDoList and ToDoTask entites with db configuration.

---
+++ SimplyToDo.Data/Entities/ToDoList.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SimplyToDo.Data.Entities;
+
+internal class ToDoList
+{
+    public int Id { get; set; }
+
+    // The navigation property is nullable even though it's required as 
+    // we may not always load the related list when querying.
+    public required string OwnerId { get; set; }
+    public AppUser? Owner { get; set; }
+
+    // A user settable background color in HTML Hex format
+    public string? BackgroundColor { get; set; }
+
+    // A user settable foreground color in HTML Hex format
+    public string? ForegroundColor { get; set; }
+
+    // Used to allow the user to hide fully completed lists.
+    public bool IsCompleted { get; set; }
+
+    // Nullable as the user may have not added any tasks to the list
+    public List<ToDoTask>? Tasks { get; set; }
+}
+
+internal class ToDoListConfiguration : IEntityTypeConfiguration<ToDoList>
+{
+    public void Configure(EntityTypeBuilder<ToDoList> builder)
+    {
+        builder.ToTable(nameof(ToDoList));
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.BackgroundColor)
+            .IsRequired(false)
+            .HasMaxLength(9);
+
+        builder.Property(p => p.ForegroundColor)
+            .IsRequired(false)
+            .HasMaxLength(9);
+
+        builder.HasOne(p => p.Owner)
+            .WithMany()
+            .HasPrincipalKey(p => p.Id)
+            .HasForeignKey(p => p.OwnerId)
+            .IsRequired(true)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ SimplyToDo.Data/Entities/ToDoTask.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SimplyToDo.Data.Entities;
+
+internal class ToDoTask
+{
+    public int Id { get; set; }
+
+    // The navigation property is nullable even though it's required as 
+    // we may not always load the related list when querying.
+    public int ToDoListId { get; set; }
+    public ToDoList? ToDoList { get; set; }
+
+    // A custom user task status
+    public string? TaskStatus { get; set; }
+
+    // A user settable background color in HTML Hex format
+    public string? BackgroundColor { get; set; }
+
+    // A user settable foreground color in HTML Hex format
+    public string? ForegroundColor { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    // TODO: Should we limit the maximum length of a task description?
+    public required string Description { get; set; }
+
+    public DateOnly? DueByDate { get; set; }
+
+    public TimeOnly? DueByTime { get; set; }
+}
+
+internal class ToDoTaskConfiguration : IEntityTypeConfiguration<ToDoTask>
+{
+    public void Configure(EntityTypeBuilder<ToDoTask> builder)
+    {
+        builder.ToTable(nameof(ToDoTask));
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.TaskStatus)
+            .IsRequired(false)
+            .HasMaxLength(32);
+
+        builder.Property(p => p.BackgroundColor)
+            .IsRequired(false)
+            .HasMaxLength(9);
+
+        builder.Property(p => p.ForegroundColor)
+            .IsRequired(false)
+            .HasMaxLength(9);
+
+        builder.Property(p => p.Description)
+            .IsRequired(true);
+
+        builder.Property(p => p.DueByDate)
+            .IsRequired(false);
+
+        builder.Property(p => p.DueByTime)
+            .IsRequired(false);
+
+        builder.HasOne(p => p.ToDoList)
+            .WithMany(p => p.Tasks)
+            .HasPrincipalKey(p => p.Id)
+            .HasForeignKey(p => p.ToDoListId)
+            .IsRequired(true)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
