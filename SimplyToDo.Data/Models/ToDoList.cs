59889631d5796f3fea8b973a6a29515b42d4aea6
travis@travestyofcode.com <travis@travestyofcode.com>
2026-06-27 18:58:49 -0400

    Adds DTO models with internal conversion from entities.

---
+++ SimplyToDo.Data/Models/ToDoList.cs
@@ -0,0 +1,32 @@
+﻿namespace SimplyToDo.Data.Models;
+
+public class ToDoList
+{
+    public int Id { get; set; }
+
+    public required string OwnerId { get; set; }
+
+    // A user settable background color in HTML Hex format
+    public string? BackgroundColor { get; set; }
+
+    // A user settable foreground color in HTML Hex format
+    public string? ForegroundColor { get; set; }
+
+    // Used to allow the user to hide fully completed lists.
+    public bool IsCompleted { get; set; }
+}
+
+internal static class ToDoListExtensions
+{
+    public static ToDoList AsEntity(this Entities.ToDoList source) =>
+        new ToDoList()
+        {
+            Id = source.Id,
+            OwnerId = source.OwnerId,
+            BackgroundColor = source.BackgroundColor,
+            ForegroundColor = source.ForegroundColor,
+            IsCompleted = source.IsCompleted
+        };
+
+    public static IQueryable<ToDoList> AsEntity(this IQueryable<Entities.ToDoList> source) => source.Select(l => l.AsEntity());
+}
---
+++ SimplyToDo.Data/Models/ToDoTask.cs
@@ -0,0 +1,44 @@
+﻿namespace SimplyToDo.Data.Models;
+
+public class ToDoTask
+{
+    public int Id { get; set; }
+
+    public int ToDoListId { get; set; }
+
+    // A custom user task status
+    public string? TaskStatus { get; set; }
+
+    // A user settable background color in HTML Hex format
+    public string? BackgroundColor { get; set; }
+
+    // A user settable foreground color in HTML Hex format
+    public string? ForegroundColor { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public required string Description { get; set; }
+
+    public DateOnly? DueByDate { get; set; }
+
+    public TimeOnly? DueByTime { get; set; }
+}
+
+internal static class ToDoTaskExtensions
+{
+    public static ToDoTask AsEntity(this Entities.ToDoTask source) =>
+        new ToDoTask()
+        {
+            Id = source.Id,
+            ToDoListId = source.ToDoListId,
+            TaskStatus = source.TaskStatus,
+            BackgroundColor = source.BackgroundColor,
+            ForegroundColor = source.ForegroundColor,
+            IsCompleted = source.IsCompleted,
+            Description = source.Description,
+            DueByDate = source.DueByDate,
+            DueByTime = source.DueByTime
+        };
+
+    public static IQueryable<ToDoTask> AsEntity(this IQueryable<Entities.ToDoTask> source) => source.Select(t => t.AsEntity());
+}
