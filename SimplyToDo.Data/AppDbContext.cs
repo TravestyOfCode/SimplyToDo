ad338fd0af1cd3903bc18406304b4726a02fcf89
travis@travestyofcode.com <travis@travestyofcode.com>
2026-06-27 18:03:14 -0400

    Adds design time factory and sets default connection for database.

--- SimplyToDo.Data/AppDbContext.cs
+++ SimplyToDo.Data/AppDbContext.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace SimplyToDo.Data;
 
@@ -7,6 +10,28 @@ internal class AppDbContext : IdentityDbContext<AppUser>
 {
     public AppDbContext(DbContextOptions options) : base(options)
     {
+    }
+}
+
+internal class DesignTimeAppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var path = Directory.GetCurrentDirectory();
+
+        var config = new ConfigurationBuilder()
+            .SetBasePath(path)
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .Build();
+
+        var connectionString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Unable to get connection string.");
+
+        var dbOptionsBuilder = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite(connectionString);
+
+        return new AppDbContext(dbOptionsBuilder.Options);
     }
 }
 
