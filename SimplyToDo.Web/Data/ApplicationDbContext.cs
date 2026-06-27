b81a037e4e600386ea5f34a1c49491fb7689b7d6
travis@travestyofcode.com <travis@travestyofcode.com>
2026-06-27 11:34:06 -0400

    Add project files.

---
+++ SimplyToDo.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimplyToDo.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
