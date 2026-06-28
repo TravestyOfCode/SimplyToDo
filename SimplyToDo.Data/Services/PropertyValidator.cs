df1c3304e7146f17087443dee22bbe5df6201db9
travis@travestyofcode.com <travis@travestyofcode.com>
2026-06-28 12:26:40 -0400

    Adds generic property validator to validate data attributes.

---
+++ SimplyToDo.Data/Services/PropertyValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimplyToDo.Data.Services;
+
+internal class PropertyValidator
+{
+    public IEnumerable<ValidationResult> ValidateModel<T>(T model) where T : notnull
+    {
+        ValidationContext context = new ValidationContext(model);
+
+        var errors = new List<ValidationResult>();
+
+        _ = Validator.TryValidateObject(model, context, errors, true);
+
+        return errors;
+    }
+}
