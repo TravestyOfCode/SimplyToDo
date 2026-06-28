40997320a567571f7cdabc85428a6b3a9597dcc8
travis@travestyofcode.com <travis@travestyofcode.com>
2026-06-28 12:26:23 -0400

    Adds a user accessor interface and implementation using HttpContextAccessor.

---
+++ SimplyToDo.Data/Services/IUserAccessor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace SimplyToDo.Data.Services;
+
+internal interface IUserAccessor
+{
+    public string? GetCurrentUser();
+}
+
+internal class HttpContextUserAccessor(IHttpContextAccessor _accessor) : IUserAccessor
+{
+    public string? GetCurrentUser() => _accessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+}
